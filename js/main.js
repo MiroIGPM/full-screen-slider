ddad8bac893213abebe5014fc2f32192bcdc2270
MiroIGPM <43861586+MiroIGPM@users.noreply.github.com>
2020-04-24 18:14:50 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,46 +5,27 @@ const auto = true;
 const interval = 5000; // interval za slider 5000 ms
 let slideInterval;
 
-
-
 const nextSlide = () => {
-
     const current = document.querySelector('.current');  
-
     current.classList.remove('current');
-
     if (current.nextElementSibling){
-
         current.nextElementSibling.classList.add('current');
     }else{
-    
         slides[0].classList.add('current'); 
-     }
-     
+     }     
      setTimeout(() => current.classList.remove('current')); 
 };
 
-
-
 const previousSlide = () => {
-
     const current = document.querySelector('.current');  
-
     current.classList.remove('current');
-
     if(current.previousElementSibling){
-
         current.previousElementSibling.classList.add('current');
     }else{
-
         slides[slides.length - 1].classList.add('current'); 
-     }
- 
-     setTimeout(() => current.classList.remove('current')); 
-     
+     } 
+     setTimeout(() => current.classList.remove('current'));     
 }
-
-
 
 next.addEventListener('click', e => {
     nextSlide();
@@ -62,10 +43,6 @@ prev.addEventListener('click', e =>{
     }
 });
 
-
-
-
 if(auto){
-
     slideInterval = setInterval(nextSlide, interval);
 }
